34f952571008b18b4a4a8ee40d4d0682a9e4b94d
Nathan Olive <65222446+Natolive@users.noreply.github.com>
2025-12-30 19:40:56 +0100

    Ajout de la table "seasons" et création de l'entité correspondante avec le repository

---
+++ backend/src/Entity/Seasons.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\IdTrait;
+use App\Entity\Trait\TimestampableTrait;
+use App\Repository\SeasonsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeasonsRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Seasons
+{
+    use IdTrait;
+    use TimestampableTrait;
+
+    #[ORM\Column]
+    private int $startYear;
+
+    #[ORM\Column]
+    private int $endYear;
+
+    public function getStartYear(): int
+    {
+        return $this->startYear;
+    }
+
+    public function setStartYear(int $startYear): static
+    {
+        $this->startYear = $startYear;
+
+        return $this;
+    }
+
+    public function getEndYear(): int
+    {
+        return $this->endYear;
+    }
+
+    public function setEndYear(int $endYear): static
+    {
+        $this->endYear = $endYear;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'startYear' => $this->getStartYear(),
+            'endYear' => $this->getEndYear(),
+            'createdAt' => $this->getCreatedAt()?->format(DATE_ATOM),
+            'updatedAt' => $this->getUpdatedAt()?->format(DATE_ATOM),
+        ];
+    }
+}
