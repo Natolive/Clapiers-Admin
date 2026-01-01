1f065b9fe077e4dd4a887626d9ec6a7e604b17f6
Nathan Olive <65222446+Natolive@users.noreply.github.com>
2026-01-01 18:38:19 +0100

    Ajout du service de toast personnalisé et amélioration de la gestion des erreurs d'authentification dans useApi

--- frontend/app/composables/useApi.ts
+++ frontend/app/composables/useApi.ts
@@ -1,5 +1,7 @@
 export function useApi() {
-    const config = useRuntimeConfig()
+    const config = useRuntimeConfig();
+    const toast = usePVToastService();
+
 
     const baseURL = config.public.apiBase
 
@@ -10,22 +12,19 @@ export function useApi() {
             if (token) options.headers.set('Authorization', `Bearer ${token}`)
         },
         onResponseError({ response }) {
-            // Check if it's an authentication error
-            if (response.status === 401) {
-                const authStore = useAuthStore()
-                authStore.logout()
-                return;
-            }
-
-            // Show error toast for other errors
             const message = response._data?.message || response.statusText || 'An error occurred'
-            const toast = useToast();
             toast.add({
                 severity: 'error',
                 summary: 'Error',
                 detail: message,
                 life: 5000
             });
+
+            // Check if it's an authentication error
+            if (response.status === 401) {
+                const authStore = useAuthStore()
+                authStore.logout();
+            }
         }
     })
 }
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,27 +1,29 @@
 import Aura from '@primeuix/themes/aura';
 
 export default defineNuxtConfig({
+    compatibilityDate: '2026-01-01',
     runtimeConfig: {
         public: {
             apiBase: 'http://127.0.0.1:8000/api', // accessible on client
         }
     },
     modules: ['@primevue/nuxt-module', '@pinia/nuxt'],
-    css: [
-        'primeflex/primeflex.css',
-        'primeicons/primeicons.css',
-        // We don't import theme CSS here anymore!
-        // It's handled by the preset below.
-    ],
     primevue: {
+        autoImport: true,
         options: {
             theme: {
                 preset: Aura,
                 options: {
                     darkModeSelector: 'light',
-                    cssLayer: false // Disabling layers often fixes "variable not found" issues in Nuxt
+                    cssLayer: false
                 }
             }
         }
-    }
+    },
+    css: [
+        'primeflex/primeflex.css',
+        'primeicons/primeicons.css',
+        // We don't import theme CSS here anymore!
+        // It's handled by the preset below.
+    ]
 })
