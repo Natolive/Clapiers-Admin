d7ac36a9b7dad10f7b460915b05f589c9d05876e
Nathan <nolive@solem.fr>
2026-05-20 08:31:09 +0200

    refactor: streamline Google Tag Manager script formatting in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -40,14 +40,11 @@ export default defineNuxtConfig({
             ],
             script: [
                 {
-                    innerHTML: `
-                        <script>
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-NGL84QZM');
-                        </script>`,
+                    innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-NGL84QZM');`,
                 },
                 {
                     type: 'application/ld+json',
@@ -82,11 +79,7 @@ export default defineNuxtConfig({
             ],
             noscript: [
                 {
-                    innerHTML: `
-                        <noscript>
-                            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGL84QZM" height="0" width="0" style="display:none;visibility:hidden">
-                            </iframe>
-                        </noscript>`,
+                    innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGL84QZM" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
                     tagPosition: 'bodyOpen',
                 },
             ],
@@ -137,4 +130,4 @@ export default defineNuxtConfig({
         '~/assets/css/global.css',
         '~/assets/css/home.css'
     ]
-})
+});
